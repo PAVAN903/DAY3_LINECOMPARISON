34b96ec72bb9dacf3070b431c253bda0da90e0be
PAVAN903 <navap903@gmail.com>
2023-02-20 16:26:57 +0530

    added equality

--- src/com/bridgelab/LineComparison.java
+++ src/com/bridgelab/LineComparison.java
@@ -7,37 +7,49 @@ public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("welcome to line comparison program");
 		double lengthofLine;
-		double lineoflength1,lineoflength2;
-		Scanner sc=new Scanner(System.in);
+		double lineoflength1, lineoflength2;
+		double l1,l2;
+		Scanner sc = new Scanner(System.in);
 		System.out.println("enter co ordinates of x1");
-		int x1=sc.nextInt();
+		int x1 = sc.nextInt();
 		System.out.println("enter co ordinates of xx2");
-		int x2=sc.nextInt();
+		int x2 = sc.nextInt();
 		System.out.println("enter co ordinates of y1");
-		int y1=sc.nextInt();
+		int y1 = sc.nextInt();
 		System.out.println("enter co ordinates of y2");
-		int y2=sc.nextInt();
-		 
-        lengthofLine=(Math.sqrt(Math.pow(x2, x1))+Math.pow(y2, y1));
-        System.out.println("Length of line is:: "+lengthofLine);
-		
+		int y2 = sc.nextInt();
+
+		lengthofLine = (Math.sqrt(Math.pow(x2, x1)) + Math.pow(y2, y1));
+		System.out.println("Length of line is:: " + lengthofLine);
+        
+		System.out.println("enter line co ordinates x1,x2 and y1,y2"); 
+		lineoflength1 = getLength(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+    	l1 = getLength(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
 		System.out.println("enter line co ordinates x1,x2 and y1,y2");
-		 lineoflength1 = getLength(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt());
-		 System.out.println("enter line co ordinates x1,x2 and y1,y2");
-		 lineoflength2 = getLength(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt());
-		System.out.println("length of line 1:: "+lineoflength1);
-		System.out.println("length of line 2:: "+lineoflength2);
-		if(lineoflength1==lineoflength2) {
+		lineoflength2 = getLength(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+		l2 = getLength(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+		System.out.println("length of line 1:: " + lineoflength1);
+		System.out.println("length of line 2:: " + lineoflength2);
+		if (lineoflength1 == lineoflength2) {
+			System.out.println("both lines are equal");
+
+		} else {
+			System.out.println("both lines are not equal");
+		}
+		if(l1>l2) {
+			System.out.println("line 1 is greater than line 2");
+		} else if(l2>l2) {
+			System.out.println("line 2 is grater than line 1");
+		}else {
 			System.out.println("both lines are equal");
 			
-		}else {
-			System.out.println("both lines are not equal");
+			
 		}
 	}
 
-	 static double getLength(int x1, int x2, int y1, int y2) {
-		
-		return (Math.sqrt(Math.pow(x2, x1))+Math.pow(y2, y1));
+	static double getLength(int x1, int x2, int y1, int y2) {
+
+		return (Math.sqrt(Math.pow(x2, x1)) + Math.pow(y2, y1));
 	}
 
 }
